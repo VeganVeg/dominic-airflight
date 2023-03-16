cb6c2569095f275840745623b5e900ba71ddf0cf
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2023-03-16 19:16:52 -0400

    f

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,11 @@
-const mySwiperOne = new Swiper(".swiper", {
+const mySwiperOne = new swiper(".swiper", {
+    direction: 'horizontal',
+    grabCursor: true,
+    effect: 'flip',
+
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: ".swiper-button-prev",
+        prevEl: ".swiper-button-next",
     },
 
 });
